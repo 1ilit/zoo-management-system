1cd9ec6b134fe53ee1b45158c6cd9ad63a3275c9
day <lilitmousheghyan@gmail.com>
2023-04-11 20:40:19 +0300

    Finish StaffInfo page

--- src/components/staff_member/StaffInfo.tsx
+++ src/components/staff_member/StaffInfo.tsx
@@ -41,7 +41,56 @@ export default function StaffInfo() {
               </h3>
             </div>
             <hr />
-            <div>{staffMember?.first_name}</div>
+            <div className="card">
+              <div className="card-top bg-success">
+                <h4 className="my-3 ms-3 text-white">
+                  SSN: {staffMember?.ssn}
+                </h4>
+              </div>
+              <div className="card-body">
+                <p className="card-text">
+                  <p>
+                    <span className="fw-bold">First name : </span>{" "}
+                    {staffMember?.first_name}
+                  </p>
+                  <p>
+                    <span className="fw-bold">Last name : </span>{" "}
+                    {staffMember?.last_name}
+                  </p>
+                  <p>
+                    <span className="fw-bold">Sex : </span> {staffMember?.sex}
+                  </p>
+                  <p>
+                    <span className="fw-bold">Address : </span>{" "}
+                    {staffMember?.address}
+                  </p>
+                  <p>
+                    <span className="fw-bold">Email : </span>{" "}
+                    {staffMember?.email}
+                  </p>
+                  <p>
+                    <span className="fw-bold">Phone number : </span>{" "}
+                    {staffMember?.phone_number}
+                  </p>
+                  <p>
+                    <span className="fw-bold">Pay per hour : </span>{" "}
+                    {staffMember?.payperhour}
+                  </p>
+                  <p>
+                    <span className="fw-bold">Pay per month : </span>{" "}
+                    {staffMember?.paypermonth}
+                  </p>
+                  <p>
+                    <span className="fw-bold">Job type : </span>{" "}
+                    {staffMember?.isvolunteer === "Y"
+                      ? "Volunteer"
+                      : staffMember?.isfulltime === "Y"
+                      ? "Part time"
+                      : "Full time"}
+                  </p>
+                </p>
+              </div>
+            </div>
           </div>
         </div>
       </div>
